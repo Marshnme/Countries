0fe41fc30f0a4caccbed73ffb4edb89ea4d2010c
Joshua Holtsclaw <jildsmiles@yahoo.com>
2021-10-05 21:40:04 -0400

    search bar basic styling. Early day for work tomorrow

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 
 const SearchBar = () => {
+
+    const Container = styled.div`
+        
+    @media (min-width:520px) {
+        display:flex;
+        flex-flow: row nowrap;
+        justify-content:space-evenly;
+        align-items:center;
+        width:90%;
+        padding: 1vh 0;
+        border:1px solid red;
+      }
+
+    `;
     return(
-        <div>
-            <p>SearchBar</p>
-        </div>
+        <Container>
+            <div>
+                 <p>Searchicon</p>
+            </div>
+            <div>
+                <input placeholder="Search for a country"></input>
+            </div>
+           
+        </Container>
     )
 }
 
