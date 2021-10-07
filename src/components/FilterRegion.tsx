dcf306b41c7c7756f4635457ec197e9548785122
Joshua Holtsclaw <jildsmiles@yahoo.com>
2021-10-07 19:42:40 -0400

    added basic layout for filterRegion and Regions.Making Api call next

--- src/components/FilterRegion.tsx
+++ src/components/FilterRegion.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 const FilterRegion = () => {
+
+    const Container = styled.div`
+        
+    @media (min-width:520px) {
+        display:flex;
+        flex-flow: row nowrap;
+        justify-content:space-evenly;
+        align-items:center;
+        width:90%;
+        padding: 1vh 0;
+        border:1px solid red;
+      }
+
+    `;
     return(
-        <div>
+        <Container>
             <p>FilterRegion</p>
-        </div>
+        </Container>
     )
 }
 
--- src/components/Regions.tsx
+++ src/components/Regions.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 const Regions = () => {
+
+    const Container = styled.div`
+        
+    @media (min-width:520px) {
+        display:flex;
+        flex-flow: row nowrap;
+        justify-content:space-evenly;
+        align-items:center;
+        width:90%;
+        padding: 1vh 0;
+        border:1px solid red;
+      }
+
+    `;
     return(
-        <div>
+        <Container>
             <p>Regions</p>
-        </div>
+        </Container>
     )
 }
 
